282586437cc614229faef8406a2d6b9cf8968dce
代码搬运工 <994301536@qq.com>
2026-06-02 09:34:00 +0800

    3.6.1-beta4

--- dispatcher/workerGeneral.ts
+++ dispatcher/workerGeneral.ts
@@ -1,7 +1,7 @@
 import ReplaceInterface from '../replace/interface'
 import WorkerReplace from '../worker/main'
-import replace from '../worker/main/general'
-// import replace from '../dist/main/general'
+// import replace from '../worker/main/general'
+import replace from '../dist/main/general'
 
 export default (concurrency?: number): ReplaceInterface => {
   return new WorkerReplace(new replace(concurrency))
